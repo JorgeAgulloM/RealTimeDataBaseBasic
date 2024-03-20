35792bd5a55b33af41ab3f2e5e6ebbc290434801
Jorge1 <agullojorge@gmail.com>
2024-03-20 20:18:29 +0100

    Configured viewBinding

--- app/src/main/java/com/softyorch/datastorebasic/MainActivity.kt
+++ app/src/main/java/com/softyorch/datastorebasic/MainActivity.kt
@@ -2,10 +2,15 @@ package com.softyorch.datastorebasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.softyorch.datastorebasic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
