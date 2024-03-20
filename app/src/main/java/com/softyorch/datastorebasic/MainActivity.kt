29e06368ef6a69df020c7d9eea5d774992177481
Jorge1 <agullojorge@gmail.com>
2024-03-20 20:41:51 +0100

    Added first example

--- app/src/main/java/com/softyorch/datastorebasic/MainActivity.kt
+++ app/src/main/java/com/softyorch/datastorebasic/MainActivity.kt
@@ -2,15 +2,25 @@ package com.softyorch.datastorebasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.softyorch.datastorebasic.data.FirebaseInstance
 import com.softyorch.datastorebasic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var  binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseInstance: FirebaseInstance
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        firebaseInstance = FirebaseInstance(this)
 
+        setUi()
+    }
+
+    private fun setUi() {
+        binding.btnId.setOnClickListener {
+            firebaseInstance.writeOnFirebase()
+        }
     }
 }
