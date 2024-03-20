634befb5b95e1c511c51e3eacb4f5ac610565fba
Jorge1 <agullojorge@gmail.com>
2024-03-20 20:08:23 +0100

    first commit

---
+++ app/src/main/java/com/softyorch/datastorebasic/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.softyorch.datastorebasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
