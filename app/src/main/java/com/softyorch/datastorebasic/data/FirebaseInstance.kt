fab56d9735a3da1e38bd70f1783f6b7ccd8eae95
Jorge1 <agullojorge@gmail.com>
2024-03-20 20:24:10 +0100

    First write example

--- app/src/main/java/com/softyorch/datastorebasic/data/FirebaseInstance.kt
+++ app/src/main/java/com/softyorch/datastorebasic/data/FirebaseInstance.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.google.firebase.FirebaseApp
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlin.random.Random
 
 class FirebaseInstance(context: Context) {
 
@@ -11,5 +12,11 @@ class FirebaseInstance(context: Context) {
 
     init {
         FirebaseApp.initializeApp(context)
+    }
+
+    fun writeOnFirebase() {
+        val myRef = database.reference
+        val randomValue: String = Random.nextInt(1, 200).toString()
+        myRef.setValue("My first write: $randomValue")
     }
 }
