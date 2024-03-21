7a9dd3968387e21946c103f6cce0b5df296c3981
Jorge1 <agullojorge@gmail.com>
2024-03-21 08:05:46 +0100

    Implemented model to write on RealtimeDB and write creating child

--- app/src/main/java/com/softyorch/datastorebasic/data/FirebaseInstance.kt
+++ app/src/main/java/com/softyorch/datastorebasic/data/FirebaseInstance.kt
@@ -5,23 +5,28 @@ import com.google.firebase.FirebaseApp
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.softyorch.datastorebasic.Todo
 import kotlin.random.Random
 
 class FirebaseInstance(context: Context) {
 
     private val database = Firebase.database
+    private val myRef = database.reference
 
     init {
         FirebaseApp.initializeApp(context)
     }
 
     fun writeOnFirebase() {
-        val myRef = database.reference
         val randomValue: String = Random.nextInt(1, 200).toString()
-        myRef.setValue("My first write: $randomValue")
+        val newItem = myRef.push()
+        newItem.setValue(getGenericTodoTaskItem(randomValue))
     }
 
     fun setupDatabaseListener(postListener: ValueEventListener) {
         database.reference.addValueEventListener(postListener)
     }
+
+    private fun getGenericTodoTaskItem(randomValue: String) =
+        Todo("Task $randomValue", "This is a cream description", false)
 }
