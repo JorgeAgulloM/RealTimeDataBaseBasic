d2989244c77a6af3ba6a4cdda1c386b1091d9566
Jorge1 <agullojorge@gmail.com>
2024-03-21 21:19:22 +0100

    [Creating Dialog] Implemented delete and done

--- app/src/main/java/com/softyorch/datastorebasic/TodoViewHolder.kt
+++ app/src/main/java/com/softyorch/datastorebasic/TodoViewHolder.kt
@@ -1,6 +1,7 @@
 package com.softyorch.datastorebasic
 
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.softyorch.datastorebasic.databinding.ItemTodoBinding
 
@@ -8,13 +9,20 @@ class TodoViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
     private val binding = ItemTodoBinding.bind(view)
 
-    fun bind(todoTask: Pair<String, Todo>, onItemSelected: (String) -> Unit) {
+    fun bind(todoTask: Pair<String, Todo>, onItemSelected: (Actions, String) -> Unit) {
         todoTask.second.apply {
             binding.apply {
                 tvTitle.text = title
                 tvDescription.text = description
                 tvReference.text = todoTask.first
-                binding.cvItem.setOnClickListener { onItemSelected(todoTask.first)}
+                binding.ivDone.setOnClickListener { onItemSelected(Actions.DONE, todoTask.first) }
+                binding.ivDelete.setOnClickListener { onItemSelected(Actions.DELETE, todoTask.first) }
+                val color = if (todoTask.second.done == true) {
+                    R.color.gold
+                } else {
+                    R.color.purple_200
+                }
+                binding.cvItem.strokeColor = ContextCompat.getColor(binding.cvItem.context, color)
             }
         }
     }
