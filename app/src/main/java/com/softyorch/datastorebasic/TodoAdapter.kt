717b582b9ae5fc3c13c1075b92e4c832785e1eab
Jorge1 <agullojorge@gmail.com>
2024-03-21 08:41:26 +0100

    [Creating RecyclerView] Implemented DiffUtil

--- app/src/main/java/com/softyorch/datastorebasic/TodoAdapter.kt
+++ app/src/main/java/com/softyorch/datastorebasic/TodoAdapter.kt
@@ -2,13 +2,27 @@ package com.softyorch.datastorebasic
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 
 class TodoAdapter(private var todoList: List<Pair<String, Todo>> = emptyList()): RecyclerView.Adapter<TodoViewHolder>() {
 
+    private val diffCallBack = object : DiffUtil.ItemCallback<Pair<String, Todo>>() {
+        override fun areItemsTheSame(oldItem: Pair<String, Todo>, newItem: Pair<String, Todo>): Boolean {
+            return oldItem.first == newItem.first
+        }
+
+        override fun areContentsTheSame(oldItem: Pair<String, Todo>, newItem: Pair<String, Todo>): Boolean {
+            return oldItem.second == newItem.second
+        }
+    }
+
+    private val asyncListDiffer = AsyncListDiffer(this, diffCallBack)
+
     fun setNewList(newList: List<Pair<String, Todo>>) {
+        asyncListDiffer.submitList(newList)
         todoList = newList
-        notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
