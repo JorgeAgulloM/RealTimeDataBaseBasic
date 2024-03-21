f88dee78607e21e07dbfa641089a2d76a8e8af9e
Jorge1 <agullojorge@gmail.com>
2024-03-21 08:08:03 +0100

    [Creating RecyclerView] Created Adapter

---
+++ app/src/main/java/com/softyorch/datastorebasic/TodoAdapter.kt
@@ -0,0 +1,25 @@
+package com.softyorch.datastorebasic
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoAdapter(private var todoList: List<Pair<String, Todo>> = emptyList()): RecyclerView.Adapter<TodoViewHolder>() {
+
+    fun setNewList(newList: List<Pair<String, Todo>>) {
+        todoList = newList
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        return TodoViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent, false)
+        )
+    }
+
+    override fun getItemCount() = todoList.size
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        holder.bind(todoList[position])
+    }
+}
