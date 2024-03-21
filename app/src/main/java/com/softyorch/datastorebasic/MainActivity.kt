75701c28c6f6586fb09f8410adfcccb934dfcff2
Jorge1 <agullojorge@gmail.com>
2024-03-21 07:24:43 +0100

    Added first reads

--- app/src/main/java/com/softyorch/datastorebasic/MainActivity.kt
+++ app/src/main/java/com/softyorch/datastorebasic/MainActivity.kt
@@ -2,6 +2,11 @@ package com.softyorch.datastorebasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.getValue
 import com.softyorch.datastorebasic.data.FirebaseInstance
 import com.softyorch.datastorebasic.databinding.ActivityMainBinding
 
@@ -16,11 +21,30 @@ class MainActivity : AppCompatActivity() {
         firebaseInstance = FirebaseInstance(this)
 
         setUi()
+        setupListeners()
     }
 
     private fun setUi() {
         binding.btnId.setOnClickListener {
             firebaseInstance.writeOnFirebase()
         }
+    }
+
+    private fun setupListeners() {
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val data: String? = snapshot.getValue<String>()
+
+                data?.let {
+                    binding.tvResult.text = it
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.i("Realtime onCanceled", error.details)
+            }
+        }
+
+        firebaseInstance.setupDatabaseListener(postListener)
     }
 }
--- app/src/main/java/com/softyorch/datastorebasic/data/FirebaseInstance.kt
+++ app/src/main/java/com/softyorch/datastorebasic/data/FirebaseInstance.kt
@@ -2,6 +2,7 @@ package com.softyorch.datastorebasic.data
 
 import android.content.Context
 import com.google.firebase.FirebaseApp
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlin.random.Random
@@ -18,5 +19,9 @@ class FirebaseInstance(context: Context) {
         val myRef = database.reference
         val randomValue: String = Random.nextInt(1, 200).toString()
         myRef.setValue("My first write: $randomValue")
+    }
+
+    fun setupDatabaseListener(postListener: ValueEventListener) {
+        database.reference.addValueEventListener(postListener)
     }
 }
